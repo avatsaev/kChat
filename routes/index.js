aa1e62b35b1dcd141e6a399d310e21340a815154
A. Vatsaev <azero79@gmail.com>
2016-05-06 00:07:25 +0200

    updated bootstrap

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Kawachat' });
 });
 
 module.exports = router
