de94077eee20ab36d7d54f2cda80aa4ff863558c
A. Vatsaev <azero79@gmail.com>
2016-05-24 17:01:14 +0200

    sub categories

--- assets/coffee/app/app.coffee
+++ assets/coffee/app/app.coffee
@@ -1,2 +1,2 @@
 
-app = angular.module('KC', ['ui.router', 'ui.bootstrap', 'ngTouch', 'ngAnimate']);
+app = angular.module('KC', ['ui.router', 'ui.bootstrap', 'ngTouch', 'ngAnimate', "ui.event", "ui.keypress"]);
--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -1,6 +1,6 @@
 var app;
 
-app = angular.module('KC', ['ui.router', 'ui.bootstrap', 'ngTouch', 'ngAnimate']);
+app = angular.module('KC', ['ui.router', 'ui.bootstrap', 'ngTouch', 'ngAnimate', "ui.event", "ui.keypress"]);
 
 console.log("chat.coffee");
 
